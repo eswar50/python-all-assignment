b7b7135f1942c9418cc94519f6674c9d8f78a466
Nithya Eswar <chantichinari92@gmail.com>
2026-06-25 20:24:19 +0530

    Add files via upload

---
+++ assignment_2.py
@@ -0,0 +1,13 @@
+#Create a simple program that uses a while loop to iterate over a range of numbers from 1 to 10
+
+num = 1
+
+while num <=10:
+    if num == 8:
+        break
+    if num % 2 != 0:
+        num += 1
+        continue
+
+    print(num)
+    num += 1
